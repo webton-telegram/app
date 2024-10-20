5e5b27f840cc75a239e1e56eacb7726906c44822
bitcoin-ethereum <bitcoin@webton.ai>
2024-10-20 22:05:55 +0900

    feat: add profile main screen

--- src/routes/profile.tsx
+++ src/routes/profile.tsx
@@ -1,9 +1,55 @@
+import { Listbox, ListboxItem } from '@nextui-org/react';
+import { FaChevronRight } from 'react-icons/fa';
+import { useNavigate } from 'react-router-dom';
+
 import LayoutContainer from 'components/layout/LayoutContainer';
 
-const Profile = () => (
-  <LayoutContainer>
-    <h2>Profile</h2>
-  </LayoutContainer>
-);
+type Key = string | number;
+
+type ListItem = {
+  key: Key;
+  text: string;
+  link: string;
+};
+
+const list: ListItem[] = [
+  {
+    key: 'recently',
+    text: 'Recently Viewed Posts',
+    link: '/profile/recently',
+  },
+  {
+    key: 'recommended',
+    text: 'Recommended Posts',
+    link: '/profile/recommended',
+  },
+];
+
+const Profile = () => {
+  const navigate = useNavigate();
+
+  const handleAction = (key: Key) => {
+    const find = list.find((item) => item.key === key);
+    if (!find) return;
+
+    navigate(find.link);
+  };
+
+  return (
+    <LayoutContainer>
+      <Listbox aria-label="Actions" onAction={handleAction}>
+        {list.map((item) => (
+          <ListboxItem
+            key={item.key}
+            color="primary"
+            endContent={<FaChevronRight size={20} />}
+          >
+            <p className="text-xl">{item.text}</p>
+          </ListboxItem>
+        ))}
+      </Listbox>
+    </LayoutContainer>
+  );
+};
 
 export default Profile;
---
+++ src/routes/recommended.tsx
@@ -0,0 +1,9 @@
+import LayoutContainer from 'components/layout/LayoutContainer';
+
+const Recommended = () => (
+  <LayoutContainer>
+    <h2>Recommended</h2>
+  </LayoutContainer>
+);
+
+export default Recommended;
