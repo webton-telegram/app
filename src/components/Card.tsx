affe526e2f077679fcbdc5f5c7688e8fc8053338
bitcoin-ethereum <bitcoin@webton.ai>
2024-10-23 15:31:27 +0900

    feat: add card component

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,5 +1,48 @@
-const Card = () => (
-  <div className="w-full h-52 bg-gray-700 text-white rounded-xl" />
+import { Card, CardBody, CardFooter, Image } from '@nextui-org/react';
+import { GrView } from 'react-icons/gr';
+import { FaThumbsUp } from 'react-icons/fa6';
+
+type Props = {
+  img: string;
+  title: string;
+  writer: string;
+  viewCount: string;
+  likeCount: string;
+};
+
+const CustomCard = ({ img, title, writer, viewCount, likeCount }: Props) => (
+  <Card
+    shadow="sm"
+    isPressable
+    onPress={() => console.log('Go to detail page')}
+  >
+    <CardBody className="overflow-visible p-0">
+      <Image
+        shadow="sm"
+        radius="lg"
+        width="100%"
+        alt={title}
+        className="w-full object-cover"
+        src={img}
+      />
+    </CardBody>
+    <CardFooter className="flex-col items-start w-full gap-1 px-1">
+      <p className="text-sm truncate w-full text-left">{title}</p>
+      <p className="text-xs truncate w-full text-left text-gray-400">
+        {writer}
+      </p>
+      <div className="flex items-center justify-between w-full">
+        <div className="flex items-center gap-1">
+          <GrView className="text-gray-400" />
+          <p className="text-xs text-gray-400">{viewCount}</p>
+        </div>
+        <div className="flex items-center gap-1">
+          <FaThumbsUp className="text-gray-400" />
+          <p className="text-xs text-gray-400">{likeCount}</p>
+        </div>
+      </div>
+    </CardFooter>
+  </Card>
 );
 
-export default Card;
+export default CustomCard;
